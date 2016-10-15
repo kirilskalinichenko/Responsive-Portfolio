dc62d6a8db06e49f0744b893d12ce563eb16a84c
kirilskalinichenko <kirilkalinichenko@KKMacBook-Air.local>
2016-10-14 20:30:36 -0500

    adding index.php additions

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php include_once("Kalinichenko Hmwrk_2 Prt_1/index.html"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/portfolio.html"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/contact.html"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/index.html"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/assets/css/style.css"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/assets/css/reset.css"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/assets/images/cat.jpg"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/assets/images/concrete_seamless.png"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/assets/images/github-128.png"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/assets/images/linkedin-128.png"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/assets/images/linkedin_pic.png"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/assets/images/stackoverflow-128.png"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/assets/images/technics -q-c-640-480-1.png"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/assets/images/technics -q-c-640-480-2.png"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/assets/images/technics -q-c-640-480-5.png"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/assets/images/technics -q-c-640-480-7.png"); ?>
+<?php include_once("Kalinichenko Hmwrk_2 Prt_1/assets/images/technics -q-c-640-480-9.png"); ?>
